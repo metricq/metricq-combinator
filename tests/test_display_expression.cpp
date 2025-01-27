d904680935cddd352b3ecedb755dee66e6fb6ace
Florian Mros <florian_alexander.mros@tu-dresden.de>
2025-01-27 13:43:16 +0100

    fix(test_display_expression): clearer test cases

--- tests/test_display_expression.cpp
+++ tests/test_display_expression.cpp
@@ -2,56 +2,58 @@
 #include <iostream>
 #include <metricq/json.hpp>
 
+struct TestDisplayExpression
+{
+    nlohmann::json input;
+    std::string expected;
+};
+
 int main()
 {
     try
     {
-        // Input test data
-        nlohmann::json data = {
-            { "testCases",
-              { { { "expression",
-                    { { "operation", "*" },
-                      { "left", 5 },
-                      { "right", { { "operation", "-" }, { "left", 45 }, { "right", 3 } } } } } },
-                { { "expression",
-                    { { "operation", "*" },
-                      { "left", { { "operation", "+" }, { "left", 1 }, { "right", 2 } } },
-                      { "right",
-                        { { "operation", "-" },
-                          { "left", 10 },
-                          { "right", "dummy.source" } } } } } },
-                { { "expression",
-                    { { "operation", "-" },
-                      { "left",
-                        { { "operation", "+" },
-                          { "left", 15.3 },
-                          { "right",
-                            { { "operation", "min" }, { "inputs", { 42, 24, 8, 12 } } } } } },
-                      { "right",
-                        { { "operation", "throttle" },
-                          { "cooldown_period", "42" },
-                          { "input", 8 } } } } } } } }
-        };
+        std::vector<TestDisplayExpression> testCases;
 
-        // Expected outputs
-        std::vector<std::string> expected = { "(5 * (45 - 3))", "((1 + 2) * (10 - dummy.source))",
-                                              "((15.300000 + min[42, 24, 8, 12]) - 8)" };
+        testCases.emplace_back(TestDisplayExpression{
+            { { "expression",
+                { { "operation", "*" },
+                  { "left", 5 },
+                  { "right", { { "operation", "-" }, { "left", 45 }, { "right", 3 } } } } } },
+            "(5 * (45 - 3))" });
+
+        testCases.emplace_back(TestDisplayExpression{
+            { { "expression",
+                { { "operation", "*" },
+                  { "left", { { "operation", "+" }, { "left", 1 }, { "right", 2 } } },
+                  { "right",
+                    { { "operation", "-" }, { "left", 10 }, { "right", "dummy.source" } } } } } },
+            "((1 + 2) * (10 - dummy.source))" });
+
+        testCases.emplace_back(TestDisplayExpression{
+            { { "expression",
+                { { "operation", "-" },
+                  { "left",
+                    { { "operation", "+" },
+                      { "left", 15.3 },
+                      { "right", { { "operation", "min" }, { "inputs", { 42, 24, 8, 12 } } } } } },
+                  { "right",
+                    { { "operation", "throttle" },
+                      { "cooldown_period", "42" },
+                      { "input", 8 } } } } } },
+            "((15.300000 + min[42, 24, 8, 12]) - 8)" });
 
         // Test the cases
-        const auto& testCases = data["testCases"];
-        for (size_t i = 0; i < testCases.size(); ++i)
+        for (const auto& testCase : testCases)
         {
-            const auto& testCase = testCases[i];
-            const auto& expression = testCase["expression"];
 
-            // Call the function to display the expression
+            auto expression = testCase.input["expression"];
             std::string result = Combinator::displayExpression(expression);
 
             // Compare with expected result
-            if (result != expected[i])
+            if (result != testCase.expected) // comparing with the expected output
             {
-                std::cerr << "Test case " << i + 1 << " failed:\n";
-                std::cerr << "Expected: " << expected[i] << "\n";
+                std::cerr << "Test case " << testCase.input << " failed:\n";
+                std::cerr << "Expected: " << testCase.expected << "\n";
                 std::cerr << "Got: " << result << "\n";
                 return 1;
             }
